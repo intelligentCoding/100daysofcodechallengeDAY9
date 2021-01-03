73afb260724a3755a7f0be55a86ea5138b2c747c
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-02 23:12:44 -0500

    ball created and detected collision with wall

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
@@ -11,7 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard1 = Scoreboard()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -26,7 +27,21 @@ while game_is_on:
 
     # detect collision with ball
     if ball.ycor() > 280 or ball.ycor() < - 280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    #  detect when right paddle is missed
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when left paddle is missed
+    if ball.xcor() < -380:
+        ball.reset_position()
+
+
 screen.exitonclick()
 
 
